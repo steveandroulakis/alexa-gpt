6f06d816de1d132ca3c10ccb6921ec3abb097723
Steve Androulakis <steve.androulakis@gmail.com>
2023-05-14 13:10:27 -0700

    express setup

---
+++ index.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+const express = require('express')
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
