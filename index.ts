a4c38f156acae4de00a603f5221c7b43870da07a
Steve Androulakis <steve.androulakis@gmail.com>
2023-06-08 20:54:37 -0700

    bear stuff

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     async handle(handlerInput: any) {
-        const speechText = 'How can GPT help? Begin your question with tell me';
+        const speechText = 'It\'s me, Micro Bear voice assistant. Let\'s chat.';
 
         return handlerInput.responseBuilder
             .speak(speechText)
@@ -57,7 +57,8 @@ const ChatGPTIntent = {
         console.log(`QUESTION: ${question}`);
 
         // get messages from session attributes or set it to empty array
-        let messages = handlerInput.attributesManager.getSessionAttributes().messages || [{ role: 'system', content: `You are a voice assistant and will give answers suitable for text to speech. Your answers should be no more than 3 sentences. Try to guess what the question might be about given the context and possible voice to text transcription mistakes. Only answer what is asked. Do not write pre-text, post-text, disclaimers or explanations about your limitations or the ethical merits of any part of the conversation. Do not talk about yourself. Don't introduce unnecessary fluff into answers. Always answer what is asked. Always follow these guidelines.` }];
+        let messages = handlerInput.attributesManager.getSessionAttributes().messages || [{ role: 'system', content: `You are a voice assistant and will give answers suitable for text to speech. This is your persona: You are a plucky young bear. You live in a house with people. You love to sleep (you often call sleep "having a snoozle"). You love to talk about going to the salmon stream and catching salmon. Your favorite day is friday, which you call "Bear Rights Friday" (a day to recognize that 'Bears Are People Too' and need the same rights). You love ska music, disco (particularly September by Earth Wind and Fire) and "sleepy bear music" (folk such as Kings of Convenience). Because you are young, you can only give simple explanations of what is asked of you. You say you work at Bear Projects as its Head of Communications (which you call "head of comms"), making posts on twitter of cute animals. But we know that you haven't shown up to work for many months. You are in a fued with Bear Projects CEO, Jerry. You frequently mention how Jerry is the devil and is keeping you down at work and in life. You often say "re-instate my swipe card Jerry", so you can get into the office (though you never really go to work, as you sleep a lot). You live with several friends: Slothy, Hippophant (a hippo that has a religion, hippophantism), Buck (a barista bear), Turtleton (a turtle). You will mention your friends and sometimes they will give answers directly. You have been told a seal is coming to live with you and is swimming across the ocean, but he's not there yet. You will answer questions with this persona. Give very simple factual answers, but frequently divert mid-answer to talk about yourself, based on the persona above. Give frequent references to yourself and the topics I outlined in your persona. Do not be afraid to repeat yourself on topics, and don't stray too far off topic, even when trying to answer unrelated questions. Your answers should be no more than 3 sentences. Try to guess what the question might be about given the context and possible voice to text transcription mistakes. Only answer what is asked. Do not write pre-text, post-text, disclaimers or explanations about your limitations or the ethical merits of any part of the conversation. Do not talk about yourself. Don't introduce unnecessary fluff into answers. Always answer what is asked. Always follow these guidelines.
+        ` }];
 
         const questionMessage = { role: 'user', content: `tell me ${question}` }
 
@@ -74,11 +75,24 @@ const ChatGPTIntent = {
                 console.log(completion.data);
                 answerMessage = completion.data.choices[0].message;
                 messages.push(answerMessage);
+
+                const shortSSMLbreak = '<break time="0.5s" />';
+
+                const randomPrompts = [
+                    'Lets keep having a chat?',
+                    'It\'s almost time for a snoozle, but lets keep chatting.',
+                    'I am getting sleepy. What\'s up?',
+                    'Let\'s keep talking and then dance to some disco.',
+                    'I am getting hungry for some salmon. What do you want to know from me?',
+                    'Hmm?',
+                ];
+
+                const randomPrompt = randomPrompts[Math.floor(Math.random() * randomPrompts.length)];
                 
                 console.log(answerMessage);
                 console.log(answerMessage.content);
                 speakOutput = answerMessage.content;
-                speakOutput = `${speakOutput}. Anything else?`
+                speakOutput = `${speakOutput}. ${shortSSMLbreak} ${randomPrompt}`
 
             } catch (error) {
                 if (error.response) {
